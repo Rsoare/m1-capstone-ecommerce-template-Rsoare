9efc09dc004856275f1b7b4b797df7f438b990b8
Rihan soares <rihan145@gmail.com>
2022-11-15 15:42:41 -0300

    projetoFinalModulo

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 const ulProdutos = document.querySelector(".list-produtos")
-
+let somapreco = 0
+let somaProdutos = 0 
 function listaDeProdutos(arrObj) {
     for (let i = 0; i < arrObj.length; i++) {
         let liProdutos = document.createElement("li")
         liProdutos.classList.add("produtos")
-        ulProdutos.appendChild(liProdutos)
-
+        
         let imgProdutos = document.createElement("img")
         imgProdutos.src = arrObj[i].img
         imgProdutos.alt = `${arrObj[i].nameItem}`
@@ -15,27 +15,29 @@ function listaDeProdutos(arrObj) {
         let pTag = document.createElement("p")
         pTag.innerHTML = `${arrObj[i].tag}`
         pTag.classList.add("tag")
-        liProdutos.appendChild(pTag)
-
+        
         let h2Produto = document.createElement("h2")
         h2Produto.innerHTML = `${arrObj[i].nameItem}`
         h2Produto.classList.add("name-item")
-        liProdutos.appendChild(h2Produto)
-
+        
         let pDescrisao = document.createElement("p")
         pDescrisao.innerHTML = `${arrObj[i].description}`
         pDescrisao.classList.add("description")
-        liProdutos.appendChild(pDescrisao)
-
+        
         let precoProduto = document.createElement("span")
         precoProduto.innerHTML = `R$ ${arrObj[i].value.toFixed(2)}`
         precoProduto.classList.add("value")
-        liProdutos.appendChild(precoProduto)
-
+        
         let botaoProduto = document.createElement("button")
         botaoProduto.innerHTML = `Adicionar ao carrinho`
         botaoProduto.id = `card${arrObj[i].id}`
         botaoProduto.classList.add("addCart")
+        
+        ulProdutos.appendChild(liProdutos)
+        liProdutos.appendChild(pTag)
+        liProdutos.appendChild(h2Produto)
+        liProdutos.appendChild(pDescrisao)
+        liProdutos.appendChild(precoProduto)
         liProdutos.appendChild(botaoProduto)
     }
 }
@@ -44,23 +46,27 @@ listaDeProdutos(data)
 const botaoProduto = document.querySelectorAll(".addCart")
 const listaCarrinho = document.querySelector(".lista-carrinho")
 
-function selecionarBotao() {
+function addCarrinho() {
     for (let i = 0; i < botaoProduto.length; i++) {
         let botao = botaoProduto[i]
 
         botao.addEventListener('click', function (evento) {
             let botaoId = evento.target.id
             let id = parseInt(botaoId.substring(4))
-            
+
             if (carrinhoDuplicado(id) == false) {
+                somaProdutos++
                 let produto = encontarCard(data, id)
                 let produtoCarrinho = criarCardcarrinho(produto)
                 listaCarrinho.appendChild(produtoCarrinho)
+                somapreco += produto.value
+                somaPreco()
+                somaQuantidade()
             }
         })
     }
 }
-selecionarBotao()
+addCarrinho()
 
 function encontarCard(arrObj, id) {
     for (let i = 0; i < arrObj.length; i++) {
@@ -80,29 +86,41 @@ function criarCardcarrinho(produto) {
     imgCarrinho.src = produto.img
     imgCarrinho.alt = `${produto.nameItem}`
     imgCarrinho.classList.add("imagem-carrinho")
-    liCarrinho.appendChild(imgCarrinho)
-
+    
     let divCarrinho = document.createElement("div")
     divCarrinho.classList.add("descricao-carrinho")
-    liCarrinho.appendChild(divCarrinho)
-
+    
     let h3Carrinho = document.createElement("h3")
     h3Carrinho.classList.add("name-item-carrinho")
     h3Carrinho.innerHTML = `${produto.nameItem}`
-    divCarrinho.appendChild(h3Carrinho)
-
+    
     let spanCarrinho = document.createElement("span")
     spanCarrinho.classList.add("preco-carrinho")
-    spanCarrinho.innerHTML = `${produto.value}`
-    divCarrinho.appendChild(spanCarrinho)
-
+    spanCarrinho.innerHTML = `R$ ${produto.value.toFixed(2)}`
+    
     let buttonCarrinho = document.createElement("button")
     buttonCarrinho.classList.add("botao-carrinho")
     buttonCarrinho.innerHTML = "Remover Produto"
-    divCarrinho.appendChild(buttonCarrinho)
 
+
+    buttonCarrinho.addEventListener('click',function(event){
+        somaProdutos--
+        let liRemover = document.querySelector("#carrinhoLi"+ produto.id)
+        liRemover.remove()
+        somapreco -= produto.value
+        somaPreco()
+        somaQuantidade()
+    })
+
+    
+    liCarrinho.appendChild(imgCarrinho)
+    liCarrinho.appendChild(divCarrinho)
+    divCarrinho.appendChild(h3Carrinho)
+    divCarrinho.appendChild(spanCarrinho)
+    divCarrinho.appendChild(buttonCarrinho)
     return liCarrinho
 }
+
 
 function carrinhoDuplicado(id) {
     let elemento = document.querySelector("#carrinhoLi" + id)
@@ -112,4 +130,15 @@ function carrinhoDuplicado(id) {
     else {
         return true
     }
+}
+
+function somaPreco() {
+    let spanTotal = document.querySelector(".totalCarrinho")
+    spanTotal.innerHTML = `R$${somapreco.toFixed(2)}`
+    console.log(spanTotal.innerHTML)
+}
+
+function somaQuantidade() {
+    let spanQuantidade = document.querySelector(".totalQuantidade")
+    spanQuantidade.innerHTML = somaProdutos
 }
